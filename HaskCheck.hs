1eea2d0dfca92f616e78cecca27a1310d8cb078c
Henry Wandover <henrywandover@gmail.com>
2024-09-16 11:21:38 -0400

    name change

--- HaskCheck.hs
+++ HaskCheck.hs
@@ -2,11 +2,23 @@
 -- @author Henry Wandover
 -- @date Sept. 7, 2024
 
+{-# LANGUAGE FlexibleInstances #-}
+
 module HaskCheck (
  hwAssertEqual,
+ hwAssert,
  hwAssertTrue,
  hwAssertFalse
 ) where
+
+-- @brief Compare lists.
+hwLstCmp :: (Num a, Eq a) => [a] -> [a] -> Bool
+hwLstCmp [] [] = True
+hwLstCmp _ [] = False
+hwLstCmp [] _ = False
+hwLstCmp (it1:list1) (it2:list2) =
+ if it1 == it2 then hwLstCmp list1 list2
+ else False
 
 {- 
  - Reference for `show`: https://hackage.haskell.org/package/base-4.20.0.1/docs/Text-Show.html
@@ -21,6 +33,21 @@ hwAssertEqual msg expected actual =
 
  else putStrLn (msg ++ " \x1b[1;91m✗\x1b[0m" ++ "\n" ++ "\t>Expected= " ++ show expected 
  ++ "\n" ++ "\t>Actual= " ++ show actual)
+
+class Assert a where
+ hwAssert :: String -> a -> a -> IO ()
+
+instance Assert Int where
+ hwAssert msg expected actual =
+  if expected == actual then putStrLn (msg ++ " \x1b[1;92m✓\x1b[0m")
+
+  else putStrLn (msg ++ " \x1b[1;91m✗\x1b[0m" ++ "\n" ++ "\t>Expected= " ++ show expected ++ "\n" ++ "\t>Actual= " ++ show actual)
+
+instance Assert [Int] where
+ hwAssert msg expected actual =
+  if hwLstCmp expected actual then putStrLn (msg ++ " \x1b[1;92m✓\x1b[0m")
+
+  else putStrLn (msg ++ " \x1b[1;91m✗\x1b[0m" ++ "\n" ++ "\t>Expected= " ++ show expected ++ "\n" ++ "\t>Actual= " ++ show actual)
 
 hwAssertTrue :: String -> Bool -> IO ()
 hwAssertTrue msg actual =
